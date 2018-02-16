bf3bff0f70dbe3c390ae3b5ac858b204c26a699c
alberto <alberto.massa@ymail.com>
2018-02-16 19:35:15 +0100

    NOT WORKING, go home

--- src/s02/index.js
+++ src/s02/index.js
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { observer } from 'mobx-react'
 import { AnimationState } from 's02/AnimationState'
+import { Transition } from 'react-transition-group'
 import 'tachyons'
 
 const store = new AnimationState()
+
+const duration = 300
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+  padding: 20,
+  display: 'inline-block',
+  backgroundColor: '#8787d8',
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+}
 
 function Header() {
   return (
@@ -20,7 +36,16 @@ class Example extends React.Component {
     const { currentSectionId } = store
     return (
       <div className="flex justify-center">
-        <h1 className="bb b--black pa2 map">Section #{currentSectionId}</h1>
+        <Transition in={inProp} timeout={duration}>
+          {(state) => (
+            <div style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}>
+              <h2 className="pa2 map">Section #{currentSectionId}</h2>
+            </div>
+          )}
+        </Transition>
       </div>
     )
   }
