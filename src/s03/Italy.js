df22f7fb5875eca5e2489939daa7e997b7b2c8b7
alberto <alberto.massa@ymail.com>
2018-02-19 17:21:09 +0100

    s03 improvements

---
+++ src/s03/Italy.js
@@ -0,0 +1,154 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import { DWARF, EXPAND } from './AnimationState'
+import Tweenr from 'tweenr'
+import { cloneDeep } from 'lodash'
+
+import { store } from './'
+
+const animationOptions = {
+  duration: 1,
+  ease: 'expoOut',
+}
+
+const mapSmallPosition = {
+  position: [20, 20],
+  radius: 10,
+  duration: 1,
+}
+
+const mapBigPosition = {
+  position: [150, 150],
+  radius: 90,
+}
+
+const linePosition1 = {
+  x1: 20,
+  y1: 20,
+  x2: 90,
+  y2: 10,
+}
+
+const linePosition2 = {
+  x1: 20,
+  y1: 20,
+  x2: 90,
+  y2: 30,
+}
+
+const linePosition3 = {
+  x1: 20,
+  y1: 20,
+  x2: 90,
+  y2: 50,
+}
+
+const lines = [linePosition1, linePosition2, linePosition3]
+
+@observer
+export class Italy extends React.Component {
+  tween = null
+  animation = null
+
+  mapRef = null
+  lineRed = null
+
+  valueMap = { ...cloneDeep(mapBigPosition) }
+  valueLine = { value: 100 }
+
+  state = {
+    map: { ...cloneDeep(mapBigPosition) },
+    line: { ...cloneDeep(this.valueLine) },
+  }
+
+  componentDidMount() {
+    this.tween = Tweenr()
+
+    this.tween.on('tick', () => {
+      const { valueMap, valueLine } = this
+
+      if (this.state.map.radius !== valueMap.radius) {
+        this.setState({ map: { ...valueMap } })
+      }
+      if (this.state.line.value !== valueLine.value) {
+        this.setState({ line: { ...valueLine } })
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.tween.dispose()
+  }
+
+  openLens = () => {
+    this.tween.to(this.valueLine, {
+      value: 0,
+      ...animationOptions,
+    })
+  }
+
+  closeLens = onComplete => {
+    this.tween.to(this.valueLine, {
+      value: 100,
+      ...animationOptions,
+    })
+    .on('complete', onComplete)
+  }
+
+  startAnimation = direction => {
+    console.log('Italy > startAnimation')
+    const { tween, valueMap } = this
+
+    if (direction === DWARF) {
+      this.animation = tween
+      .cancel()
+      .to(valueMap, { ...cloneDeep(mapSmallPosition), ...animationOptions })
+      .on('complete', () => {
+        this.animation = null
+        this.openLens()
+        store.endTransitioning()
+      })
+    } else if (direction === EXPAND) {
+      this.closeLens(() => {
+        this.animation = tween
+        .cancel()
+        .to(valueMap, { ...cloneDeep(mapBigPosition), ...animationOptions })
+        .on('complete', () => {
+          this.animation = null
+          store.endTransitioning()
+        })
+      })
+    }
+  }
+
+  render() {
+    const { isTransitioning, direction } = store
+
+    if (this.mapRef && isTransitioning && this.animation === null) {
+      this.startAnimation(direction)
+    }
+
+    const { currentSectionId } = store
+    const currentLine = currentSectionId === 0
+      ? {}
+      : lines[currentSectionId - 1]
+
+    return (
+      <g>
+        <circle
+          ref={ref => { this.mapRef = ref }}
+          cx={this.state.map.position[0]}
+          cy={this.state.map.position[1]}
+          r={this.state.map.radius}
+        />
+        <line
+          {...currentLine}
+          strokeDasharray="100"
+          strokeDashoffset={this.state.line.value}
+          fillOpacity="0"
+          stroke="red"
+        />
+      </g>
+    )
+  }
+}
--- src/s03/index.js
+++ src/s03/index.js
@@ -1,94 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { observer } from 'mobx-react'
-import { AnimationState, DWARF, EXPAND } from './AnimationState'
-import ReactTransitionGroup from 'react-addons-transition-group'
+import { AnimationState } from './AnimationState'
 import 'tachyons'
-import Tweenr from 'tweenr'
-import { noop } from 'lodash'
+import { Italy } from './Italy'
 
-const store = new AnimationState()
-
-const animationOptions = {
-  duration: 1,
-  ease: 'expoOut',
-}
-
-@observer
-class Italy extends React.Component {
-  tween = null
-  ref = null
-  animation = null
-  value = {
-    position: [150, 150],
-    radius: 90,
-  }
-  state = { ...this.value }
-
-  componentDidMount() {
-    this.tween = Tweenr()
-
-    this.tween.on('tick', () => {
-      const { value } = this
-      if (this.state.radius !== value.radius) {
-        this.setState(value)
-      }
-    })
-  }
-
-  componentWillUnmount() {
-    this.tween.dispose()
-  }
-
-  startAnimation = direction => {
-    console.log('Italy > startAnimation')
-    const { tween, value } = this
-    if (direction === DWARF) {
-      this.animation = tween
-      .cancel()
-      .to(value, {
-        position: [20, 20],
-        radius: 10,
-        duration: 1,
-      })
-      .on('complete', () => {
-        this.animation = null
-        store.endTransitioning()
-      })
-    } else if (direction === EXPAND) {
-      this.animation = tween
-      .cancel()
-      .to(value, {
-        position: [150, 150],
-        radius: 90,
-        duration: 1,
-      })
-      .on('complete', () => {
-        this.animation = null
-        store.endTransitioning()
-      })
-    }
-  }
-
-  render() {
-    const { isTransitioning, direction } = store
-
-    if (this.ref && isTransitioning && this.animation === null) {
-      this.startAnimation(direction)
-    }
-
-    return (
-      <g>
-        <circle
-          ref={ref => { this.ref = ref }}
-          cx={this.state.position[0]}
-          cy={this.state.position[1]}
-          r={this.state.radius}
-        />
-      </g>
-    )
-  }
-}
+export const store = new AnimationState()
 
 @observer
 class Example extends React.Component {
@@ -109,7 +26,6 @@ class Example extends React.Component {
               <button onClick={this.buildSetSection(1)}>1</button>
               <button onClick={this.buildSetSection(2)}>2</button>
               <button onClick={this.buildSetSection(3)}>3</button>
-              <button onClick={this.buildSetSection(4)}>4</button>
             </div>)
             : (<div>
               <button onClick={this.buildSetSection(0)}>exit</button>
